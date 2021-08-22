d67d8fcfb8f37413cb5590b150ea04f960f18369
Sirawit <popeye-zz@hotmail.com>
2021-08-22 19:09:43 +0700

    display a list v.1

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,16 +9,21 @@ import { HEROES } from '../mock-heroes'
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // }
 
   heroes = HEROES;
-
+  selectedHero?: Hero;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
   }
 
 }
