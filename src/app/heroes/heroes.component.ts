77c1d3f87d29667bdabbe907800e772736d30a6c
Sirawit <popeye-zz@hotmail.com>
2021-08-26 22:24:14 +0700

    navigation v.2.0

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -16,18 +16,18 @@ export class HeroesComponent implements OnInit {
   // }
 
   heroes: Hero[] = [];
-  selectedHero?: Hero;
+  // selectedHero?: Hero;
 
-  constructor(private heroService: HeroService, private messageService:MessageService) { }
+  constructor(private heroService: HeroService) { } //private messageService:MessageService
 
   ngOnInit(): void {
     this.getHeroes(); // this class.getHerores
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-  }
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+  // }
 
   getHeroes(): void {
     // this.heroes = this.heroService.getHeroes(); // get heroes from service into this.heroes variable
