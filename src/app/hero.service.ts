c4abca72f37401b59498f2f519c18d46e7e94d0a
Sirawit <popeye-zz@hotmail.com>
2021-08-28 16:33:32 +0700

    success http.get from in-memory-data api

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,17 +4,30 @@ import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messageService: MessageService) { }
+
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
 
   getHeroes(): Observable<Hero[]> {
     //Hero[]
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    
+    //RxJs 'of()'
+    // const heroes = of(HEROES);
+    // this.messageService.add('HeroService: fetched heroes');
+    // return heroes;
+
+    //Http
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> { //return with Observable<Hero>
@@ -24,5 +37,10 @@ export class HeroService {
     const hero = HEROES.find(h => h.id === id)!; // why it need '!' 
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero); // make Observable
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,11 +3,9 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Hero } from './hero';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InMemoryDataService implements InMemoryDbService {
-
-  //THIS IS DATABASE ! ! !
   createDb() {
     const heroes = [
       { id: 11, name: 'Dr Nice' },
@@ -21,7 +19,7 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 19, name: 'Magma' },
       { id: 20, name: 'Tornado' }
     ];
-    return (heroes);
+    return {heroes};
   }
 
   // Overrides the genId method to ensure that a hero always has an id.
@@ -29,14 +27,7 @@ export class InMemoryDataService implements InMemoryDbService {
   // the method below returns the initial number (11).
   // if the heroes array is not empty, the method below returns the highest
   // hero id + 1.
-  
   genId(heroes: Hero[]): number {
-    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11; // ah shit wtf ? 
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
   }
-  
-  
-  
-  
-  
-  //constructor() { }
 }
